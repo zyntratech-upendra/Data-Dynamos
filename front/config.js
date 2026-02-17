a1e678a00290e2baf337071bc71caf9ee1af9ea0
Pranav <pranavgudimella09@gmail.com>
2026-02-17 16:31:16 +0530

    changd to fronte

--- front/config.js
+++ front/config.js
@@ -2,7 +2,7 @@ window.__CONFIG__ = {
   API_URL:
     window.location.hostname === 'localhost'
       ? 'http://localhost:5000/api'
-      : 'https://https://data-dynamos-5tdq.onrender.com/api',
+      : 'https://data-dynamos-5tdq.onrender.com/api',
 
   getApiUrl() {
     return this.API_URL;
